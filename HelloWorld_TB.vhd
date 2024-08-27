f02947b5f391abc573d1b9b6d54f843d33b1561f
Sara Pereira <130683805+saraajp@users.noreply.github.com>
2024-08-27 22:09:44 +0100

    Update HelloWorld_TB.vhd

--- HelloWorld_TB.vhd
+++ HelloWorld_TB.vhd
@@ -2,7 +2,7 @@
 --architecture: onde pomos a logica/algoritemos
 --process: basicamente, onde escrevemos "o codigo a serio"
 --wait: para o execuçao do codigo
---report: é um printf
+--report: é um printf  
 
 entity HelloWorld_TB is  --pomos o nome do ficheiro TestBench (TB)
 end entity;
@@ -15,7 +15,9 @@ begin
 
 		report "Hello World";
 		wait;
-
+		
+		--Quando o process acaba, faz loop infinito, daí precisarmos do wait
+		--Podemos tbm escrever wait for <tempo> ns --> e acada <tempo> faz loop do hello world
 	end process;
 
 end architecture;
