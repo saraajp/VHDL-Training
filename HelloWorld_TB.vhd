6ac9f341dd80bb726b42f192f5aed2e8d182a13a
Sara Pereira <130683805+saraajp@users.noreply.github.com>
2024-08-27 01:09:56 +0100

    Update HelloWorld_TB.vhd

--- HelloWorld_TB.vhd
+++ HelloWorld_TB.vhd
@@ -1,7 +1,8 @@
 --entity: define os input e outputs de um módulo
 --architecture: onde pomos a logica/algoritemos
 --process: basicamente, onde escrevemos "o codigo a serio"
-
+--wait: para o execuçao do codigo
+--report: é um printf
 
 entity HelloWorld_TB is  --pomos o nome do ficheiro TestBench (TB)
 end entity;
